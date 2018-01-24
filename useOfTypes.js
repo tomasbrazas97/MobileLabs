b3084b42c18d85055c8cb16482cc0b3f264352bb
TOMAS BRAZAS - STUDENT <G00349242@gmit.ie>
2018-01-24 17:40:52 +0000

    some helpful files

--- useOfTypes.js
+++ useOfTypes.js
@@ -1,1 +1,47 @@
-"use strict";
+//Demonstrates the use of types
+//boolean
+var flag = true;
+console.log("Value assigned to flag is: " + flag);
+//number
+var decimal = 6;
+console.log("Value assigned to decimal is: " + decimal);
+//String
+var color = "blue";
+color = 'red';
+console.log("Value assigned to color is: " + color);
+//array
+var list = [1, 2, 3];
+for (var i = 0; i < list.length; i++) {
+    console.log("Number " + (i + 1) + " is: " + list[i] + ".");
+}
+//tuple
+var x;
+x = ["hello", 10];
+console.log("Tuple example: " + x[0].substr(1));
+//enum
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Green"] = 2] = "Green";
+    Color[Color["Blue"] = 3] = "Blue";
+})(Color || (Color = {}));
+var colorName = Color[2];
+var c = Color.Green;
+console.log("Enum: Value of colorName is: " + colorName);
+console.log("Enum: Value of c is: " + c);
+console.log("Enum: Name of c is: " + Color[c]);
+//any
+var notSure = 4;
+notSure = "maybe a string instead";
+console.log("Value is a string: " + notSure + ".");
+notSure = false;
+console.log("Now value is a boolean: " + notSure + ".");
+//any array
+var listany = [1, true, "free"];
+for (var i = 0; i < listany.length; i++) {
+    console.log("Before: Entry " + (i + 1) + " is: " + listany[i] + ".");
+}
+listany[1] = 100;
+for (var i = 0; i < listany.length; i++) {
+    console.log("After: Entry " + (i + 1) + " is: " + listany[i] + ".");
+}
---
+++ useOfTypes.ts
@@ -0,0 +1,49 @@
+//Demonstrates the use of types
+
+//boolean
+let flag: boolean = true;
+console.log("Value assigned to flag is: " + flag);
+
+//number
+let decimal: number = 6;
+console.log("Value assigned to decimal is: " + decimal);
+
+//String
+let color: string = "blue";
+color = 'red';
+console.log("Value assigned to color is: " + color);
+
+//array
+let list: number[] = [1, 2, 3];
+for (let i = 0; i < list.length; i++) {
+    console.log("Number " + (i + 1) + " is: " + list[i] + ".");
+}
+//tuple
+let x: [string, number];
+x = ["hello", 10];
+console.log("Tuple example: " + x[0].substr(1));
+
+//enum
+enum Color { Red = 1, Green, Blue }
+let colorName: string = Color[2];
+let c: Color = Color.Green;
+console.log("Enum: Value of colorName is: " + colorName);
+console.log("Enum: Value of c is: " + c);
+console.log("Enum: Name of c is: " + Color[c]);
+
+//any
+let notSure: any = 4;
+notSure = "maybe a string instead";
+console.log("Value is a string: " + notSure + ".");
+notSure = false;
+console.log("Now value is a boolean: " + notSure + ".");
+
+//any array
+let listany: any[] = [1, true, "free"];
+for (let i = 0; i < listany.length; i++) {
+    console.log("Before: Entry " + (i + 1) + " is: " + listany[i] + ".");
+}
+listany[1] = 100;
+for (let i = 0; i < listany.length; i++) {
+    console.log("After: Entry " + (i + 1) + " is: " + listany[i] + ".")
+}
